793bd0f09ea090df1e19c7a13411e628cf4e7ce6
vkumar-99 <vimal2707@gmail.com>
2025-12-07 00:22:30 +0530

    Catalog service skeleton completed

---
+++ catalog-service/src/main/java/com/vkumar99/bookstore/catalog/CatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.vkumar99.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogServiceApplication.class, args);
+    }
+}
---
+++ catalog-service/src/test/java/com/vkumar99/bookstore/catalog/TestCatalogServiceApplication.java
@@ -0,0 +1,12 @@
+package com.vkumar99.bookstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+}
